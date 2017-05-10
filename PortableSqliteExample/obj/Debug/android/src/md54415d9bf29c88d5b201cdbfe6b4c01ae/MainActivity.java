34905dcb05a2609cfe4145033faf92de82bb1f00
Brunogr <Brunogrbhg@gmail.com>
2017-05-10 18:26:10 -0300

    xamarin-portablesqlite-sample
    An sample of how to create a sqlite repository for Xamarin.Android,
    Xamarin.iOS and Windows 8/10 projects.
    Includes Insert, Delete and Get
    samples.

---
+++ PortableSqliteExample/obj/Debug/android/src/md54415d9bf29c88d5b201cdbfe6b4c01ae/MainActivity.java
@@ -0,0 +1,94 @@
+package md54415d9bf29c88d5b201cdbfe6b4c01ae;
+
+
+public class MainActivity
+	extends android.support.v7.app.AppCompatActivity
+	implements
+		mono.android.IGCUserPeer,
+		android.widget.AdapterView.OnItemClickListener,
+		android.widget.TextView.OnEditorActionListener
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"n_onPrepareOptionsMenu:(Landroid/view/Menu;)Z:GetOnPrepareOptionsMenu_Landroid_view_Menu_Handler\n" +
+			"n_onOptionsItemSelected:(Landroid/view/MenuItem;)Z:GetOnOptionsItemSelected_Landroid_view_MenuItem_Handler\n" +
+			"n_onBackPressed:()V:GetOnBackPressedHandler\n" +
+			"n_onItemClick:(Landroid/widget/AdapterView;Landroid/view/View;IJ)V:GetOnItemClick_Landroid_widget_AdapterView_Landroid_view_View_IJHandler:Android.Widget.AdapterView/IOnItemClickListenerInvoker, Mono.Android, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null\n" +
+			"n_onEditorAction:(Landroid/widget/TextView;ILandroid/view/KeyEvent;)Z:GetOnEditorAction_Landroid_widget_TextView_ILandroid_view_KeyEvent_Handler:Android.Widget.TextView/IOnEditorActionListenerInvoker, Mono.Android, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null\n" +
+			"";
+		mono.android.Runtime.register ("PortableSqliteExample.MainActivity, PortableSqliteExample, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity () throws java.lang.Throwable
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("PortableSqliteExample.MainActivity, PortableSqliteExample, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+
+	public boolean onPrepareOptionsMenu (android.view.Menu p0)
+	{
+		return n_onPrepareOptionsMenu (p0);
+	}
+
+	private native boolean n_onPrepareOptionsMenu (android.view.Menu p0);
+
+
+	public boolean onOptionsItemSelected (android.view.MenuItem p0)
+	{
+		return n_onOptionsItemSelected (p0);
+	}
+
+	private native boolean n_onOptionsItemSelected (android.view.MenuItem p0);
+
+
+	public void onBackPressed ()
+	{
+		n_onBackPressed ();
+	}
+
+	private native void n_onBackPressed ();
+
+
+	public void onItemClick (android.widget.AdapterView p0, android.view.View p1, int p2, long p3)
+	{
+		n_onItemClick (p0, p1, p2, p3);
+	}
+
+	private native void n_onItemClick (android.widget.AdapterView p0, android.view.View p1, int p2, long p3);
+
+
+	public boolean onEditorAction (android.widget.TextView p0, int p1, android.view.KeyEvent p2)
+	{
+		return n_onEditorAction (p0, p1, p2);
+	}
+
+	private native boolean n_onEditorAction (android.widget.TextView p0, int p1, android.view.KeyEvent p2);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
---
+++ PortableSqliteExample/obj/Debug/android/src/mono/android/app/ApplicationRegistration.java
@@ -0,0 +1,11 @@
+package mono.android.app;
+
+public class ApplicationRegistration {
+
+	public static void registerApplications ()
+	{
+				// Application and Instrumentation ACWs must be registered first.
+		mono.android.Runtime.register ("PortableSqliteExample.AppStartup, PortableSqliteExample, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", md54415d9bf29c88d5b201cdbfe6b4c01ae.AppStartup.class, md54415d9bf29c88d5b201cdbfe6b4c01ae.AppStartup.__md_methods);
+		
+	}
+}
